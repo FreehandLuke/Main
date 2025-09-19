598039023c422772f65a61699704891af7692dc1
FreehandLuke <harveyallendickson@gmail.com>
2025-09-19 08:03:23 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Thanks for reaching out! This form doesn\'t send emails yet, but you can connect it to a service like Formspree or Netlify Forms.');
+});
